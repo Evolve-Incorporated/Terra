1a9d2fa2df5eca1ae651a1c48b95be2d0667fce3
wielqus <38864045+Wielqus@users.noreply.github.com>
2021-04-02 15:40:29 +0200

    Map generator

---
+++ Terra/Assets/GridManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField]
+    private int size = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    private void GenerateGrid(){
+        GameObject referenceTile = (GameObject)Instantiate(Resources.Load("GrassTile"));
+
+        for(int row = 0; row < size; row++){
+            for(int col = 0; col < size; col++){
+                GameObject tile = (GameObject) Instantiate(referenceTile, transform);
+
+                float posX = col;
+                float posY = row;
+
+                tile.transform.position = new Vector2(posX, posY);
+            }
+        }
+
+        Destroy(referenceTile);
+
+        float gridW = size;
+        float gridH = size;
+        transform.position = new Vector2(-gridW / 2 , gridH / 2);
+    }
+}
