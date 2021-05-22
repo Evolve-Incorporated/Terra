f2996604755cd1d1c23e30b8be513e4be269a1b4
wielqus <38864045+Wielqus@users.noreply.github.com>
2021-05-22 14:47:44 +0200

    Add start simulation script

--- Terra/Assets/Scripts/UI.cs
+++ Terra/Assets/Scripts/UI.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour
 {
     public void ExitGame(){
         Application.Quit();
+    }
+
+    public void StartSimulation(){
+        SceneManager.LoadScene("SampleScene");
     }
 }
