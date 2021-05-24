33d510bba6d0eeb6138463906dc2c22b97f7d929
wielqus <38864045+Wielqus@users.noreply.github.com>
2021-05-24 14:27:14 +0200

    Start simulation button

--- Terra/Assets/Scripts/UI.cs
+++ Terra/Assets/Scripts/UI.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour
 {
+
+    public GameObject map;
+
     public void ExitGame(){
+        Debug.Log("exit game");
         Application.Quit();
     }
 
     public void StartSimulation(){
-        SceneManager.LoadScene("SampleScene");
+        Debug.Log("start simuulation");
+        Instantiate(map, new Vector3(0, 0, 0), Quaternion.identity);
+        Destroy(gameObject);
     }
 }
